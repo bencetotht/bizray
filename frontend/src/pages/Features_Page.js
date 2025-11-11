9a2ce35ca2c97261546d910b707afa739e1efc5e
doraszekely <>
2025-11-11 18:49:38 +0100

    UI improvements: fixing things

--- frontend/src/pages/Features_Page.js
+++ frontend/src/pages/Features_Page.js
@@ -6,7 +6,7 @@ import Features from '../components/Features';
 
 export default function FeaturesPage() {
   return (
-    <div style={{ marginTop: "80px", marginBottom: "80px" }}>
+    <div style={{ marginTop: "80px" }}>
       <Features/>
     </div>
   );
