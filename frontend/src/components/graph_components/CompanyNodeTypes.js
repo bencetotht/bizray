784a0a1b2352ead0846009924678992395e71c99
floriankoehl <florian.v.koehl@gmail.com>
2025-11-22 17:11:36 +0100

    company nodes can now be toggled between different display types

---
+++ frontend/src/components/graph_components/CompanyNodeTypes.js
@@ -0,0 +1,163 @@
+import { Handle } from "reactflow";
+import StoreIcon from '@mui/icons-material/Store';
+import { ExternalLink } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import OpenInFullIcon from '@mui/icons-material/OpenInFull';
+import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
+
+
+
+
+function MainCompanyDisplay({ data, selected }) {
+    return (
+        <>
+            <div className={`
+${selected ? 'bg-gradient-to-br from-indigo-400 to-purple-500' : 'bg-gradient-to-br from-indigo-300 to-purple-400'}
+h-20 w-60 
+flex
+p-2
+rounded
+shadow-xl
+
+
+
+
+!p-2
+flex
+justify-center
+items-center
+text-xl
+relative
+  rounded-2xl 
+  bg-purple-500/15
+  backdrop-blur-xl 
+  border border-white/30 
+  shadow-xl
+  p-6
+
+
+            `}>
+                <h1 className="m-2 ">{data.label}</h1>
+                <Handle type="source" position="bottom" />
+                <Handle type="target" position="top" />
+            </div>
+        </>
+    )
+}
+
+function DefaultCompanyDisplay({ id, selected, data }) {
+    const navigate = useNavigate();
+
+    return (
+        <div 
+        onDoubleClick={(e)=>{
+                e.stopPropagation();
+                data.changeNodeType(id, "minimal_company_display");
+            }}
+        className={`
+${selected ? 'bg-blue-500/20 shadow-xl shadow-black/20' : 'bg-blue-400/15 '}
+h-32
+w-45
+rounded
+shadow-xl
+!p-3
+flex
+flex-col
+gap-2
+justify-center
+items-center
+text-black
+
+relative
+  rounded-2xl 
+  
+  backdrop-blur-xl 
+  border border-white/30 
+  shadow-xl
+  p-6
+
+                    
+                    
+                    `}>
+            {data.label}
+            <Handle type="source" position="bottom" />
+            <Handle type="target" position="top" />
+            <div className="flex gap-2 justify-center">
+                <div
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        console.log("Expand triggered, i am : ", id)
+                        data.fetchCompany(id)
+
+                    }}
+                    className="bg-white p-2 w-8 h-8 flex justify-center items-center rounded hover:bg-gray-200">
+
+                    <OpenInFullIcon className="!text-[25px]" />
+                </div>
+                <div
+                    onClick={(e) => {
+                        e.stopPropagation();
+
+                        console.log("Collapse triggered, i am : ", id)
+                        data.collapseCompany(id);
+                    }}
+                    className="bg-white p-2 w-8 h-8 flex justify-center items-center rounded hover:bg-gray-200">
+                    <CloseFullscreenIcon className="!text-[25px]" />
+                </div>
+                {/* Navigate to /graph/:id */}
+        <button
+          onClick={(e) => {
+            e.stopPropagation();       
+            navigate(`/graph/${id}`);   
+          }}
+          className="inline-flex items-center gap-2 px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700"
+        >
+          <span className="flex items-center gap-2">
+            Network
+            <ExternalLink size={16} />
+          </span>
+        </button>
+
+
+            </div>
+
+        </div>
+    );
+}
+
+
+
+function DetailedCompanyDisplay({}){
+
+}
+
+function MinimalCompanyDisplay({id, selected, data}){
+    return (
+        <>
+            <div 
+            onDoubleClick={(e)=>{
+                e.stopPropagation();
+                data.changeNodeType(id, "default_company_display");
+            }}
+            className="h-15 w-15 bg-white rounded-full flex justify-center items-center shadow-xl shadow-black/20">
+                <StoreIcon className="!text-[35px]"/>
+                <Handle type="source" position="bottom" />
+                <Handle type="target" position="top" />
+                
+               
+            </div>
+        </>
+    )
+}
+
+
+
+
+
+
+
+export const companyNodeTypes = {
+  default_company_display: DefaultCompanyDisplay,
+  main_company_display: MainCompanyDisplay,
+  minimal_company_display: MinimalCompanyDisplay,
+};
--- frontend/src/components/graph_components/Graph.js
+++ frontend/src/components/graph_components/Graph.js
@@ -22,6 +22,7 @@ import { useNavigate } from "react-router-dom";
 
 import SettingsButton from "./GraphSettings";
 import StoreIcon from '@mui/icons-material/Store';
+import { companyNodeTypes } from "./CompanyNodeTypes";
 
 
 
@@ -154,135 +155,6 @@ function starLayout(nodes, mainNodeId) {
 
 
 
-function MainCompanyDisplay({ data, selected }) {
-    return (
-        <>
-            <div className={`
-${selected ? 'bg-gradient-to-br from-indigo-400 to-purple-500' : 'bg-gradient-to-br from-indigo-300 to-purple-400'}
-h-20 w-60 
-flex
-p-2
-rounded
-shadow-xl
-
-
-
-
-!p-2
-flex
-justify-center
-items-center
-text-xl
-relative
-  rounded-2xl 
-  bg-purple-500/15
-  backdrop-blur-xl 
-  border border-white/30 
-  shadow-xl
-  p-6
-
-
-            `}>
-                <h1 className="m-2 ">{data.label}</h1>
-                <Handle type="source" position="bottom" />
-                <Handle type="target" position="top" />
-            </div>
-        </>
-    )
-}
-
-function DefaultCompanyDisplay({ id, selected, data }) {
-    const navigate = useNavigate();
-
-    return (
-        <div className={`
-${selected ? 'bg-blue-500/20 shadow-xl shadow-black/20' : 'bg-blue-400/15 '}
-h-32
-w-45
-rounded
-shadow-xl
-!p-3
-flex
-flex-col
-gap-2
-justify-center
-items-center
-text-black
-
-relative
-  rounded-2xl 
-  
-  backdrop-blur-xl 
-  border border-white/30 
-  shadow-xl
-  p-6
-
-                    
-                    
-                    `}>
-            {data.label}
-            <Handle type="source" position="bottom" />
-            <Handle type="target" position="top" />
-            <div className="flex gap-2 justify-center">
-                <div
-                    onClick={(e) => {
-                        e.stopPropagation();
-                        console.log("Expand triggered, i am : ", id)
-                        data.fetchCompany(id)
-
-                    }}
-                    className="bg-white p-2 w-8 h-8 flex justify-center items-center rounded hover:bg-gray-200">
-
-                    <OpenInFullIcon className="!text-[25px]" />
-                </div>
-                <div
-                    onClick={(e) => {
-                        e.stopPropagation();
-
-                        console.log("Collapse triggered, i am : ", id)
-                        data.collapseCompany(id);
-                    }}
-                    className="bg-white p-2 w-8 h-8 flex justify-center items-center rounded hover:bg-gray-200">
-                    <CloseFullscreenIcon className="!text-[25px]" />
-                </div>
-                {/* Navigate to /graph/:id */}
-        <button
-          onClick={(e) => {
-            e.stopPropagation();        // don't let React Flow steal the click
-            navigate(`/graph/${id}`);   // tell React Router to change route
-          }}
-          className="inline-flex items-center gap-2 px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700"
-        >
-          <span className="flex items-center gap-2">
-            Network
-            <ExternalLink size={16} />
-          </span>
-        </button>
-
-
-            </div>
-
-        </div>
-    );
-}
-
-
-
-function DetailedCompanyDisplay({}){
-
-}
-
-function MinimalCompanyDisplay({id, selected, data}){
-    return (
-        <>
-            <div className="h-15 w-15 bg-white rounded-full flex justify-center items-center shadow-xl shadow-black/20">
-                <StoreIcon className="!text-[35px]"/>
-                <Handle type="source" position="bottom" />
-            <Handle type="target" position="top" />
-            </div>
-        </>
-    )
-}
 
 
 
@@ -290,11 +162,15 @@ function MinimalCompanyDisplay({id, selected, data}){
 
 
 
-const nodeTypes = {
-    default_company_display: DefaultCompanyDisplay,
-    main_company_display: MainCompanyDisplay,
-    minimal_company_display: MinimalCompanyDisplay
-}
+
+
+
+
+
+
+
+
+
 
 
 function DetailedEdge(props) {
@@ -373,6 +249,22 @@ export default function Graph({ id_that_was_passed }) {
     const [visibleNodeIds, setVisibleNodeIds] = useState(() => new Set());
     const [childrenByParent, setChildrenByParent] = useState({});
     const [rootId, setRootId] = useState(null);
+
+
+
+    function changeSingleNodeType(nodeId, newType) {
+        setNodes((prevNodes) =>
+            prevNodes.map((node) =>
+                node.id === nodeId
+                    ? { ...node, type: newType }
+                    : node
+            )
+        );
+    }
+
+
+
+
 
 
 
@@ -516,10 +408,8 @@ export default function Graph({ id_that_was_passed }) {
 useEffect(() => {
   setNodes((prevNodes) =>
     prevNodes.map((node) => {
-      // keep main display as-is
-      if (node.type === "main_company_display") return node;
+      if (node.type === "main_company_display" || node.selected) return node;
 
-      // only change company nodes
       if (
         node.type === "default_company_display" ||
         node.type === "minimal_company_display"
@@ -645,6 +535,7 @@ useEffect(() => {
                     collapseCompany: (id) => {
                         collapseCompany(id);
                     },
+                    changeNodeType: (id, newType) => changeSingleNodeType(id, newType),
                 },
             })),
         [nodes, childrenByParent]
@@ -663,7 +554,7 @@ useEffect(() => {
                             edges={displayEdges}
                             onNodesChange={onNodesChange}
                             onEdgesChange={onEdgesChange}
-                            nodeTypes={nodeTypes}
+                            nodeTypes={companyNodeTypes}
                             edgeTypes={edgeTypes}
                             fitView
                             fitViewOptions={{ padding: 0.3 }} 
